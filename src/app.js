321cb6a3a5bfc71c09604673f8bdb5d7a5713783
Vikas Pathak <123vikas.vp79@gmail.com>
2023-04-30 21:10:24 +0530

    check assignment test case

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const { off } = require('../models/user.js');
-const users   =require("../models/user.js");
-
+const mongoose = require("mongoose");
+const { off } = require("../models/user.js");
+const users = require("../models/user.js");
 
 // Import routes
 
 //Router Middlewares
 app.use(express.json());
 
-
 //default value for limit is 5 and offset is 0
 //This route should return an array of _id of all the element that need to be returned.
 //output id can be in any order.
 
-app.get("/",async function(req,res){
+app.get("/", async function (req, res) {
+  var ids = [];
 
-    var ids = [];
+  //Write your code here
+  //modify the ids array
 
-    //Write your code here
-    //modify the ids array
+  const limit = req.query.limit ? parseInt(req.query.limit) : 5;
+  const offset = req.query.offset ? parseInt(req.query.offset) : 0;
 
-    res.send(ids);
+  const usersArray = await users
+    .find({})
+    .skip(limit * offset)
+    .limit(limit)
+    .select("_id");
 
+  const ids = usersArray.map((user) => user._id);
+
+  res.send(ids);
 });
 
 module.exports = app;
-
-
